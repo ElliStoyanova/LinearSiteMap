201a8fa33233a976f933fb033fe6fc45d64e2faa
ElliStoyanova <elli.debelli@gmail.com>
2025-02-13 12:05:29 +0200

    initial map display test

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,15 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: { lat: 42.6975, lng: 23.3242 } // Example: Sofia, Bulgaria
+    });
+
+    const kmlLayer = new google.maps.KmlLayer({
+        url: "https://github.com/ElliStoyanova/LinearSiteMap/blob/main/doc.kml",  // Replace with your actual KML file URL
+        map: map
+    });
+
+    console.log('KML layer: ', kmlLayer)
+}
